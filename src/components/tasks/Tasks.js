327b44def3f800c0401434782fa8d1b2af31a7f4
Spencer Lott <spklott@gmail.com>
2023-05-09 14:37:25 -0400

    abandoning the idea of a delete all button

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useEffect } from "react"
 import { InlineEdit } from "./TaskInlineEdit"
-import { Button } from "react-bootstrap"
+import { Button, OverlayTrigger, Tooltip } from "react-bootstrap"
 
 
 
@@ -48,7 +48,12 @@ export const Tasks = ( {task, updateTasks} ) => {
 
 const deleteButton = () => {
     if (task.completed === true) {
-        return <Button 
+        return <>
+        <OverlayTrigger
+        placement="top"
+        overlay={<Tooltip>Delete</Tooltip>}
+        >
+        <Button 
         style={{backgroundColor: "transparent",
                 border: "none"}}
         className="ticket__delete"
@@ -66,6 +71,8 @@ const deleteButton = () => {
             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
           </svg></Button>
+          </OverlayTrigger>
+          </>
     }
     else {
         return ""
